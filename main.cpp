6710405d6595444b8b2c479cb7a0282b6768820e
Filcer <gabfiltzer@gmail.com>
2016-09-11 19:36:37 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
-
-
 int main ()
 {
+fibonaccirec()
+}
 
+
+int fibonacci(int n)
+{
+  if(n<2)
+    return n;
+for(i=1;i<n;++i)
+  {
+    c=a+b;
+    a=b;
+    b=c;
+  }
+  return c;
+}
+
+signed int fibonaccirec(int n)
+{
+  if(n<2){
+  return n;
+  }
+return fibonacci(n-2)+fibonacci(n-1);
+  
 }
